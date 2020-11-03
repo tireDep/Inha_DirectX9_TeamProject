c299ae28f458fac1c2946cd58ef2ed09f427328f
EdocDyne <bandyck@gmail.com>
2020-11-03 17:27:34 +0900

    -ing Object Tool
    - 모달리스 창 생성
    - Apply까지만 적용

---
+++ DxTeamPorject/DxObjectTool/DxObjectTool/DxObjectTool/MainGame.cpp
@@ -0,0 +1,28 @@
+#include "stdafx.h"
+#include "MainGame.h"
+
+CMainGame::CMainGame()
+{
+}
+
+CMainGame::~CMainGame()
+{
+	g_pDeviceManager->Destroy();
+}
+
+void CMainGame::WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+}
+
+void CMainGame::Setup()
+{
+}
+
+void CMainGame::Update()
+{
+}
+
+void CMainGame::Render()
+{
+
+}
---
+++ DxTeamPorject/DxObjectTool/DxObjectTool/DxObjectTool/MainGame.h
@@ -0,0 +1,14 @@
+#pragma once
+class CMainGame
+{
+private:
+
+public:
+	CMainGame();
+	~CMainGame();
+
+	void WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+	void Setup();
+	void Update();
+	void Render();
+};
