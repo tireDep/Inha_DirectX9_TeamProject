4d16930ffeb6b9e709aa3a250dbb9ec5c44754a3
DESKTOP-RC8S5LR\User <aadd306@naver.com>
2020-11-05 17:01:18 +0900

    -fin 프록시 수정

--- DxTeamProject/DxTeamProject/DxTeamProject.cpp
+++ DxTeamProject/DxTeamProject/DxTeamProject.cpp
@@ -168,8 +168,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             // Parse the menu selections:
             switch (wmId)
             {
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
                 break;
             default:
                 return DefWindowProc(hWnd, message, wParam, lParam);
