36f8c9d82febe100fd5b67e24f5111aff0c43075
_tireDep <wing997@naver.com>
2020-11-26 15:41:44 +0900

    -fin/맵툴 : 구 피킹 관련 코드 변경

--- DxTeamProject_MapTool/DxTeamProject_MapTool/Sphere.cpp
+++ DxTeamProject_MapTool/DxTeamProject_MapTool/Sphere.cpp
@@ -33,12 +33,11 @@ void CSphere::Update(CRay * ray)
 	D3DXVECTOR3* pVertices;
 
 	m_pMesh->LockVertexBuffer(D3DLOCK_READONLY, (void**)&pVertices);
-	D3DXVECTOR3 m_vMin, m_vMax;
-	D3DXComputeBoundingSphere(pVertices, m_pMesh->GetNumVertices(), m_pMesh->GetNumBytesPerVertex(), &m_vMin, &m_vScale.x);
-	// later.. rotation add
-	m_vMin += m_vTranslate;				m_vMax += m_vTranslate;
-
-	if (D3DXSphereBoundProbe(&m_vMin, m_vScale.x, &ray->GetOrigin(), &ray->GetDirection()) == true)
+	D3DXVECTOR3 center;
+	center = m_vTranslate;
+	float radius;
+	radius = m_vScale.x;
+	if (D3DXSphereBoundProbe(&center, radius, &ray->GetOrigin(), &ray->GetDirection()) == true)
 	{
 		cout << "picked" << endl;
 	}
