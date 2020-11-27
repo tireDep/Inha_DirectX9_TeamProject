74ed7d3a32f960d927842b5499ccbb97a44959ca
_tireDep <wing997@naver.com>
2020-11-28 01:00:47 +0900

    -fin : 클라이언트 파싱 추가(배경 오브젝트)
    - 나무 파싱 추가
    -> CObject

---
+++ DxTeamProject/DxTeamProject/Background.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "Object.h"
+
+class CBackground : public CObject
+{
+public:
+	CBackground();
+	virtual ~CBackground();
+
+	virtual void Setup() { };
+	virtual void Update(float duration) { };
+	virtual void Update(float duration, CHeight* pMap) { };
+	virtual void Update(CRay ray, D3DXCOLOR& playerColor, vector<bool>& vecIsPick, vector<D3DXVECTOR3>& vecVPos);
+	virtual void SetPickState(bool set) { };
+	virtual void OutlineRender() { }
+
+	virtual string GetName() { return m_strName; }
+	virtual void ReceiveEvent(ST_EVENT eventMsg) { };
+
+	// tmp physics
+	virtual void SetMass(const float mass) { };
+	virtual float GetMass() const { return float(); };
+	virtual bool hasFiniteMass() const { return bool();  };
+	virtual void SetPusingForce(D3DXVECTOR3 forcedirection) { };
+	virtual void AddForce(const D3DXVECTOR3 & force) { };
+	virtual void ClearAccumulator() { };
+	virtual void Integrate(float duration) { };
+	virtual void RunPhysics(float duration) { };
+	virtual bool hasIntersected(CObject * otherobject) { return bool(); };
+	virtual void CollisionOtherObject(CObject * otherobject) { };
+
+	virtual void Setup(ST_MapData setData);
+	virtual void Render();
+};
+
