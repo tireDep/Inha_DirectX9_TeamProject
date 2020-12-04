109580eab878362fef79678cc8258205d03344fa
EdocDyne <bandyck@gmail.com>
2020-12-05 00:19:48 +0900

    -ing 기믹. 회전하는 판자
    - Setup. 파일 로드 부분 고치기
    - 맵 툴 인자들 예슬씨랑 상의

--- DxTeamProject/DxTeamProject/RotationBoard.cpp
+++ DxTeamProject/DxTeamProject/RotationBoard.cpp
@@ -6,10 +6,10 @@ RotationBoard::RotationBoard()
 	, m_adjBuffer(NULL)
 	, m_numMtrls(0)
 	//, rotationAxial(RotationAxial::NONE)
-	// Test
+	// Test Y Rotation
 	, rotationAxial(RotationAxial::Rotation_Y)
-	, m_fRotationSpeed(0.1)
-	, m_fRot(0.0f)
+	, m_fRotationSpeed(1.0)
+	, m_fRotAngle(0.0f)
 {
 	D3DXMatrixIdentity(&m_matWorld);
 	D3DXMatrixIdentity(&m_matRotGimmick);
@@ -30,7 +30,6 @@ void RotationBoard::Setup(string folder, string file)
 		MessageBox(g_hWnd, L"LoadXFile Fail", L"Error", MB_OK);
 		return;
 	}
-
 	m_pMesh = xfile->pMesh;
 	m_adjBuffer = xfile->adjBuffer;
 	m_vecMtrls = xfile->vecMtrl;
@@ -45,16 +44,22 @@ void RotationBoard::Update(float duration)
 	switch (rotationAxial)
 	{
 	case RotationAxial::Rotation_X:
-		m_fRot += m_fRotationSpeed * duration;
-		D3DXMatrixRotationX(&m_matRotGimmick, m_fRot);
+		m_fRotAngle += m_fRotationSpeed * duration;
+		if (m_fRotAngle > 2 * D3DX_PI)
+			m_fRotAngle -= 2 * D3DX_PI;
+		D3DXMatrixRotationX(&m_matRotGimmick, m_fRotAngle);
 		break;
 	case RotationAxial::Rotation_Y:
-		m_fRot += m_fRotationSpeed * duration;
-		D3DXMatrixRotationY(&m_matRotGimmick, m_fRot);
+		m_fRotAngle += m_fRotationSpeed * duration;
+		if (m_fRotAngle > 2 * D3DX_PI)
+			m_fRotAngle -= 2 * D3DX_PI;
+		D3DXMatrixRotationY(&m_matRotGimmick, m_fRotAngle);
 		break;
 	case RotationAxial::Rotation_Z:
-		m_fRot += m_fRotationSpeed * duration;
-		D3DXMatrixRotationZ(&m_matRotGimmick, m_fRot);
+		m_fRotAngle += m_fRotationSpeed * duration;
+		if (m_fRotAngle > 2 * D3DX_PI)
+			m_fRotAngle -= 2 * D3DX_PI;
+		D3DXMatrixRotationZ(&m_matRotGimmick, m_fRotAngle);
 		break;
 	case RotationAxial::NONE:
 		D3DXMatrixIdentity(&m_matRotGimmick);
@@ -69,16 +74,12 @@ void RotationBoard::Render()
 	if (g_pD3DDevice)
 	{
 		g_pD3DDevice->SetRenderState(D3DRS_LIGHTING, false);
-		//D3DXMATRIXA16 matWorld;
-		//D3DXMatrixIdentity(&matWorld);
-
-		//D3DXMATRIXA16 matS, matT;
-		//D3DXMatrixScaling(&matS, 0.03f, 0.03f, 0.03f);
-		//D3DXMatrixTranslation(&matT, -25, 0, 0);
-		//matWorld = matS * matT;
 
 		// Test
-		m_matWorld = m_matRotGimmick;
+		D3DXMATRIXA16 matS, matT;
+		D3DXMatrixScaling(&matS, 0.03f, 0.03f, 0.03f);
+		D3DXMatrixTranslation(&matT, -25, 0, 0);
+		m_matWorld = matS * m_matRotGimmick * matT;
 		g_pD3DDevice->SetTransform(D3DTS_WORLD, &m_matWorld);
 		if (m_pMesh == NULL)
 			return;
