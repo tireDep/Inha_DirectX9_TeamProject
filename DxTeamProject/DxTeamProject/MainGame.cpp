0d25b61503692bf508f0b9d22734c941385f5869
_tireDep <wing997@naver.com>
2020-12-30 15:56:09 +0900

    -fin : 무빙큐브 이동 수정
    - 무빙 큐브 범위 이동 수정

--- DxTeamProject/DxTeamProject/MainGame.cpp
+++ DxTeamProject/DxTeamProject/MainGame.cpp
@@ -138,10 +138,8 @@ void CMainGame::Update()
 
 // >> MapData
 #ifdef _DEBUG
-		// g_pFileLoadManager->FileLoad_MapData("Resource/MapData/KTMapData", "KT_A_map1.dat");
-
-		g_pFileLoadManager->FileLoad_MapData("Resource/MapData/KTMapData", "KT_A_map1.dat");
 		g_pFileLoadManager->FileLoad_MapData("Resource/MapData/KTMapData", "KT_A_map2.dat");
+		g_pFileLoadManager->FileLoad_MapData("Resource/MapData/KTMapData", "KT_A_map1.dat");
 #else
 
 		g_pFileLoadManager->FileLoad_MapData("Resource/MapData", "tile_ocean_full1.dat");
@@ -176,8 +174,8 @@ void CMainGame::Update()
 		g_pFileLoadManager->FileLoad_MapData("Resource/MapData", "tile_ocean_full1.dat");
 		g_pFileLoadManager->FileLoad_MapData("Resource/MapData", "tile_ocean_full1.dat");
 		g_pFileLoadManager->FileLoad_MapData("Resource/MapData/KTMapData", "KT_W_map7.dat");
-		g_pFileLoadManager->FileLoad_MapData("Resource/MapData/KTMapData", "KT_W_map8.dat");
-		g_pFileLoadManager->FileLoad_MapData("Resource/MapData/KTMapData", "KT_W_map9.dat");
+		g_pFileLoadManager->FileLoad_MapData("Resource/MapData/KTMapData", "KT_A_map1.dat");
+		g_pFileLoadManager->FileLoad_MapData("Resource/MapData/KTMapData", "KT_A_map2.dat");
 		g_pFileLoadManager->FileLoad_MapData("Resource/MapData", "tile_ocean_full1.dat");
 		g_pFileLoadManager->FileLoad_MapData("Resource/MapData", "tile_ocean_full1.dat");
 		g_pFileLoadManager->FileLoad_MapData("Resource/MapData", "tile_ocean_full1.dat");
